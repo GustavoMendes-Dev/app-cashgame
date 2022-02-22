6234cf8373f9f6eadb4baa667db4b24e207fd398
Gustavo Mendes <g.mendes93@hotmail.com>
2022-02-22 19:50:30 -0300

    bugfix: delete dd in movements

--- app/Http/Controllers/MovementController.php
+++ app/Http/Controllers/MovementController.php
@@ -249,7 +249,7 @@ class MovementController extends Controller
     public function destroy($id)
     {
         $movement = $this->movements->findOrFail($id);
-        dd($movement->match);
+        // dd($movement->match);
 
         $movement->player->balance = $movement->value - ( - $movement->player->balance);
         $movement->player->save();
